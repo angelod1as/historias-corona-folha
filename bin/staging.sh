a672cf18b01f1a3019fde8b7501556c733761db1
Pilker <rogerio.luiz@grupofolha.com.br>
2020-04-03 20:06:59 -0300

    correção no título

--- bin/staging.sh
+++ bin/staging.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-osascript -e 'tell app "Finder" to open location "smb://:@marvel.grupofolha.intranet/staging"'
-mkdir -p /Volumes/staging/arte.folha.com.br/equilibrioesaude/2020/coronavirus-aqueles-que-perdemos/
+osascript -e 'tell app "Finder" to open location "smb://328954:folh%402017@marvel.grupofolha.intranet/staging"'
+mkdir -p /Volumes/staging/arte.folha.com.br/equilibrioesaude/2020/as-historias-das-vitimas-do-novo-coronavirus/
 
 printf '\n\e[1;33m%-6s\e[m\n' "┌-----------------------------------┐"
 printf '\e[1;33m%-6s\e[m\n' "|                                   |"
@@ -18,8 +18,8 @@ yarn build
 printf '\n\e[5;33m%-6s\e[m\n' "-------------------------------------"
 printf '\e[5;33m%-6s\e[m' "          Copiando projeto           "
 printf '\n\e[5;33m%-6s\e[m\n\n' "-------------------------------------"
-cp -r build/* /Volumes/staging/arte.folha.com.br/equilibrioesaude/2020/coronavirus-aqueles-que-perdemos/
+cp -r build/* /Volumes/staging/arte.folha.com.br/equilibrioesaude/2020/as-historias-das-vitimas-do-novo-coronavirus/
 
 
 printf '\n\e[5;33m%-6s\e[m\n' "Url:"
-printf '\e[1;33m%-6s\e[m\n\n' "http://staging.arte.folha.com.br/equilibrioesaude/2020/coronavirus-aqueles-que-perdemos/"
+printf '\e[1;33m%-6s\e[m\n\n' "http://staging.arte.folha.com.br/equilibrioesaude/2020/as-historias-das-vitimas-do-novo-coronavirus/"
